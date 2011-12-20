b93216882b3d6b5126d43481902fd9cc9d0a77c5
andiSVN <andiSVN@3ad16473-98a3-553c-6fcf-bbc89fc00ce8>
2011-12-20 12:14:30 +0000

    Programm debuggt, richtige Ergebnisse scheint zu funktionieren
    ToDo: Java hEap space pro für große probleme
    ToDo: visaulisierung(optional)
    toDo: Anpassung für Laufzeitmessung!

--- trunk/src/main/java/programm/de/knapsack/Main.java
+++ trunk/src/main/java/programm/de/knapsack/Main.java
@@ -8,15 +8,25 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] werte = { 2, 7, 6, 8, 4, 1, 4};
+		final int kapazitaet = 35;
+		final int maxWert=10;
+		final int maxGewicht=10;
+		final int anzahl=45;
 		int[] gewichte = {1, 4, 7, 11, 6, 2, 8};
+		int[] werte = { 2, 7, 6, 8, 4, 1, 4};
+	
 		
-		Rucksack def = new Rucksack(werte, gewichte, 15);
+		//Rucksack def = new Rucksack(werte, gewichte, Kapazitaet);
 		
-		Rucksack root = new Rucksack(20, 10, 5, 25);
+		Rucksack def = new Rucksack(anzahl, maxWert, maxGewicht, kapazitaet);
 		
 		System.out.println("Ausgangszustand Rucksackinhalte:");
 		def.print();
+		System.out.println("OpbereSchranke ="+def.obereSchranke());
+		System.out.println("UntereSchranke ="+def.untereSchranke());
+		
+		
+		
 		
 		BranchAndBound Algo = new BranchAndBound(def);
 		Algo.start();
